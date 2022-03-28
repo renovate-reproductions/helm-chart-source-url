f6fdffff265bce237210b65af593f4cfb1c6677c
Michael Kriese <michael.kriese@visualon.de>
2022-03-28 10:27:53 +0200

    chore: add test file

---
+++ prometheus.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "prometheus" {
+  name      = "kube-prometheus"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "kube-prometheus"
+  version    = "6.8.0"
+}
